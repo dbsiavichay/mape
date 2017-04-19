faaebb53f05ed547c73efed309946f3a599ec7f1
Denis Siavichay <dbsiavichay@gmail.com>
2017-04-19 18:22:26 -0500

    Register user ok

---
+++ social/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from .views import *
+
+urlpatterns = [        
+    #url(r'^login/$', auth_views.login, {'template_name': 'security/login.html',}),
+    url(r'^$', UserCreateView.as_view(), name='signup'),
+    #url(r'^logout/$', logout),
+    #url(r'^user/profile/$', login_required(UserProfileView.as_view()), name='user_profile'),
+    #url(r'^user/profile/(?P<pk>\d+)/$', login_required(UserProfileDetailView.as_view()), name='user_profile_detail'),
+    #url(r'^user/change-avatar/$', change_user_avatar),
+]
--- social/views.py
+++ social/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.shortcuts import render, redirect
+#from django.contrib.auth import logout as auth_logout, login as auth_login
+from django.contrib.auth.models import User
+from django.views.generic import CreateView, UpdateView, DetailView
+#from django.forms import modelform_factory
+from .models import *
+from .forms import *
 
-from django.shortcuts import render
+class UserCreateView(CreateView):
+	model = User
+	form_class = UserCreationForm
+	success_url = '/'
+	template_name= 'social/signup.html'
 
-# Create your views here.
+	#def form_valid(self, form):	   
+	#	self.object = form.save()
+	#	auth_login(self.request, self.object)
+	#	return redirect(self.get_success_url())
