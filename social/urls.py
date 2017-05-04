2da099982febe6377e55a7c7e76ee46dfb13cf23
Zolo-2000 <juan.s.avila@gmail.com>
2017-05-04 18:29:08 -0500

    Login+Template listo
    Definido el login con su diseño

--- social/urls.py
+++ social/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = [        
-    url(r'^login/$', auth_views.login, {'template_name': 'social/login.html',}, name='login'),
-    url(r'^$', UserCreateView.as_view(), name='signup'),
+    url(r'^$', auth_views.login, {'template_name': 'social/login.html',}, name='login'),
+    url(r'^signup/$', UserCreateView.as_view(), name='signup'),
     #url(r'^logout/$', logout),
     #url(r'^user/profile/$', login_required(UserProfileView.as_view()), name='user_profile'),
     #url(r'^user/profile/(?P<pk>\d+)/$', login_required(UserProfileDetailView.as_view()), name='user_profile_detail'),
