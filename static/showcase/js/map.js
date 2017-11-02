ba1c3cf4b339d53a37d80df0c5075fcb4ea8139e
zolo-2000 <solovino.16.2012@gmail.com>
2017-11-01 21:26:28 -0500

    mapa sin navegador

--- static/showcase/js/map.js
+++ static/showcase/js/map.js
@@ -2,8 +2,7 @@ $(function () {
 	//Funcionalidad para API Geolocation
 	//navigator.geolocation.getCurrentPosition(success, error);
 
-	function success(position) {
-	//	var point = position.coords;
+		//var point = position.coords;
 		var token = 'pk.eyJ1IjoiZGJzaWF2aWNoYXkiLCJhIjoiY2l1aDhzanVzMDExeDJ5cDR4bWtsbHA3ZCJ9.uL7b4pcnOVe1B3I0am59kQ';
 
 		// $('#btn-event-register-float').attr('lat', point.latitude);
@@ -13,7 +12,7 @@ $(function () {
 	 //  	$('#btn-locality-register-float').attr('lng', point.longitude);
 
 		L.mapbox.accessToken = token;
-		var map = L.mapbox.map('map', 'mapbox.light').setView([40, -74.50], 15);		
+		var map = L.mapbox.map('map', 'mapbox.light').setView([-2.2986156360633974, -78.12206268310548], 15);		
 		 
 		 $.get('/localities/', function(data) {	
 
@@ -69,8 +68,7 @@ $(function () {
 		  	$('#btn-locality-register').attr('lng', e.latlng.lng);
 
 		  	$('#event-modal').modal('open');
-		});	  
-	};
+		});	 
 
 	function error(error) {
 	  console.warn('ERROR(' + error.code + '): ' + error.message);
